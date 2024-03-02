cde3f4510fdebeff6c634855f866c09f42663634
JMiguelS0t0 <juanmiguelsoto4@gmail.com>
2024-03-02 12:44:12 -0500

    Base

---
+++ API_S4/API_S4/Data/Conexion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_S4.Data
+{
+    public class Conexion
+    {
+        public static string rutaConexion = "Data Source =DBPRUEBAS1; Initial Catalog = USUARIO; Integrated Security = True";
+    }
+}
